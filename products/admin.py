074f0bae48d8b2ebd5a99058728fe27f20dfec0c
Charles Enright <c.enright@me.com>
2024-09-06 17:17:17 +0000

    Customise admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Product, Season
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Season)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'season',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Season, SeasonAdmin)
