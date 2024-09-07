4345a3a9a5b22c87bb67c6d6a0512dbf9c89aa5c
Charles Enright <c.enright@me.com>
2024-09-07 17:10:25 +0000

    Add recommendations to product details and views

--- products/views.py
+++ products/views.py
@@ -23,8 +23,13 @@ def product_detail(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id) # get only one product using the product id.
 
+    # Get the recommended products by IDs
+    recommended_ids = product.recommendations.split(",")  # Convert the string to a list
+    recommended_products = Product.objects.filter(id__in=recommended_ids)  # Get the recommended products
+
     context = {
         'product': product,
+        'recommended_products': recommended_products,
     }
 
     return render(request, 'products/product_detail.html', context)
