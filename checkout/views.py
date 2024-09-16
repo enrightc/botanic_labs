3963cedeb81769b9cc7e270900dd77e6a8cc9779
Charles Enright <c.enright@me.com>
2024-09-16 15:50:22 +0000

    Add stripe elements for taking payments

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Po09mP0uuJh6VgEt4P1qYzxH4hjcXi5Ptcw5M3lcLJISvP4gol8oG7oHQnL1sJnjEx9IoeSqaU6y3nJXt1iuh5v00AbNcNUJM',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
