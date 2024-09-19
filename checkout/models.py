ec25a82b13f81d82966186a9218ff08165c5736d
Charles Enright <c.enright@me.com>
2024-09-19 12:23:56 +0000

    Update checkout form to include country field as drop-down box to prevent users entering incorrect country codes

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import Sum # used to calculate the total of all line items in an order
 from django.conf import settings # imports free delivery threshold and standard delivery percentage from project settings
 
+from django_countries.fields import CountryField
+
 from products.models import Product
 
 # This defines the model which represents the customers order
@@ -12,7 +14,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label="Country *", max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
