39236659e1eae7c319e0456c9648b6adc68e1897
Charles Enright <c.enright@me.com>
2024-09-21 14:38:02 +0000

    Add product view, url and html template

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Category
+from .models import Product, Season
 
 
 class ProductForm(forms.ModelForm):
@@ -10,9 +10,9 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        seasons = Season.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in seasons]
 
-        self.fields['category'].choices = friendly_names
+        self.fields['season'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
