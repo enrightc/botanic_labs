0fcea95db998eec1f9ad81c1a1f66d318bf69844
Charles Enright <c.enright@me.com>
2024-10-01 18:53:29 +0000

    Feat: Add functionality to allow admin to edit/delete FAQs

--- faq/urls.py
+++ faq/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.faqs, name='faq'),
     path('add/', views.add_faq, name='add_faq'),
+    path('delete/<int:id>/', views.delete_faq, name='delete_faq'),
+    path('edit/<int:id>/', views.edit_faq, name='edit_faq'),  
 ]
--- faq/views.py
+++ faq/views.py
@@ -30,6 +30,7 @@ def add_faq(request):
         if form.is_valid():
             faq = form.save(commit=False) # Create faq instance but don't save to DB yet
             faq.save()  # Now save the faq form
+            request.session['show_bag_summary'] = False
             messages.success(request, 'Successfully added FAQ!')
             return redirect(reverse('faq'))
         else:
@@ -44,3 +45,55 @@ def add_faq(request):
 
     return render(request, template, context)
 
+
+@login_required # Django will check whether the user is logged in before executing the view.
+def delete_faq(request, id):
+    """ Delete a FAQ """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+        
+    faq = get_object_or_404(Faq, id=id)
+    faq.delete()
+    request.session['show_bag_summary'] = False
+    messages.success(request, 'FAQ deleted!')
+    return redirect(reverse('faq'))
+
+
+@login_required # Django will check whether the user is logged in before executing the view.
+def edit_faq(request, id):
+    """ 
+    View to allow admin to edit an FAQ
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    
+    faq = get_object_or_404(Faq, id=id)
+
+    if request.method == 'POST':
+        form = FaqForm(request.POST, request.FILES, instance=faq)
+
+        if form.is_valid():
+            form.save()
+            request.session['show_bag_summary'] = False
+            messages.success(request, 'Successfully updated article!')
+            return redirect(reverse('faq'))
+        else:
+            messages.error(request, 'Failed to update faq. Please ensure the form is valid.')
+
+    else:
+        form = FaqForm(instance=faq)
+        messages.info(request, f'You are editing "{faq.question}"')
+
+    # Set the template to use for rendering the form
+    template = 'faq/edit_faq.html'
+
+    # Prepare the context with the form and product to send to the template
+    context = {
+        'form': form,
+        'faq': faq,
+    }
+
+    # Render the edit product template with the context data
+    return render(request, template, context)
