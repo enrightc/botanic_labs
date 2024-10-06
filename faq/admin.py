73f1aa784025603bd423503cb893a3da834151c9
Charles Enright <c.enright@me.com>
2024-10-06 18:05:27 +0000

    Feat: Add modal to confirm FAQ deletion to prevent accidental deletion

--- faq/admin.py
+++ faq/admin.py
@@ -6,4 +6,4 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Faq)
 class ArticleAdmin(SummernoteModelAdmin):
     summernote_fields = ('answer',)
-    list_display = ('question',)
+    list_display = ('question', 'id')
