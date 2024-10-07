bd9ea568e6d541eecb05b07c4dfe836902aa244e
Charles Enright <c.enright@me.com>
2024-10-07 17:24:49 +0000

    Save experiment with filters on filter-experiment branch

--- products/filters.py
+++ products/filters.py
@@ -1,5 +1,7 @@
 # filters.py
+
 import django_filters
+from django.forms.widgets import CheckboxSelectMultiple
 from .models import Product
 
 class ProductFilter(django_filters.FilterSet):
@@ -7,7 +9,15 @@ class ProductFilter(django_filters.FilterSet):
     lifespan = django_filters.ChoiceFilter(choices=Product.LIFESPAN_CHOICES)
     light_exposure = django_filters.ChoiceFilter(choices=Product.LIGHT_EXPOSURE_CHOICES)
     soil_drainage = django_filters.ChoiceFilter(choices=Product.SOIL_DRAINAGE_CHOICES)
+    
+    # Use CheckboxSelectMultiple for planting season
+    planting_season = django_filters.MultipleChoiceFilter(
+        field_name='planting_start',
+        choices=Product.MONTH_CHOICES,
+        widget=CheckboxSelectMultiple,
+        label='Planting Season'
+    )
 
     class Meta:
         model = Product
-        fields = ['type', 'lifespan', 'light_exposure', 'soil_drainage']
+        fields = ['type', 'lifespan', 'light_exposure', 'soil_drainage', 'planting_season']
