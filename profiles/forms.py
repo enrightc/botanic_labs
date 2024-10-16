059cdb609e8e7c719ecca8f8222a865cf6d9c003
Charles Enright <c.enright@me.com>
2024-10-16 16:58:43 +0000

    Add hover and active effects to navigation options and remove unused CSS

--- profiles/forms.py
+++ profiles/forms.py
@@ -32,7 +32,7 @@ class UserProfileForm(forms.ModelForm):
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = (
-                'border-black rounded-0 profile-form-input'
+                'rounded-1 profile-form-input'
             )
             self.fields[field].label = False
 
