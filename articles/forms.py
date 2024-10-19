3aee063063b4c5986b24957ac1d3baed317653c5
Charles Enright <c.enright@me.com>
2024-10-19 17:55:40 +0000

    Fix dupicate ID isssue in CustomClearableFileInput by adding custom class for image input

--- articles/forms.py
+++ articles/forms.py
@@ -12,7 +12,9 @@ class ArticleForm(forms.ModelForm):
     image = forms.ImageField(
         label='Image',
         required=False,
-        widget=ArticleCustomClearableFileInput
+        widget=ArticleCustomClearableFileInput(attrs={
+            'class': 'new-image border-black rounded-0'
+        })
         )
 
     class Meta:
@@ -41,4 +43,5 @@ class ArticleForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            if field_name != 'image':  # Skip image field as its already set its class
+                field.widget.attrs['class'] = 'border-black rounded-0'
--- products/forms.py
+++ products/forms.py
@@ -12,7 +12,9 @@ class ProductForm(forms.ModelForm):
     image = forms.ImageField(
         label='Image',
         required=False,
-        widget=CustomClearableFileInput
+         widget=CustomClearableFileInput(attrs={
+            'class': 'new-image border-black rounded-0'
+        })
     )
 
     def __init__(self, *args, **kwargs):
@@ -22,4 +24,5 @@ class ProductForm(forms.ModelForm):
 
         self.fields['season'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            if field_name != 'image':  # Skip image field as its already set its class
+                field.widget.attrs['class'] = 'border-black rounded-0'
