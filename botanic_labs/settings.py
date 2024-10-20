1643fd2e94962ed2b0f4da3678c8baa0a6f57387
Charles Enright <c.enright@me.com>
2024-10-20 15:01:18 +0000

    Add SUMMERNOTE_CONFIG to overwrite default width of input textarea and prevent it overflowing

--- botanic_labs/settings.py
+++ botanic_labs/settings.py
@@ -67,77 +67,13 @@ INSTALLED_APPS = [
     'django_filters',
 ]
 
-CKEDITOR_5_CONFIGS = {
-    'default': {
-        'toolbar': [
-            'heading', '|', 'bold', 'italic', 'link', 'bulletedList',
-            'numberedList', 'blockQuote', 'imageUpload',
-        ],
-        'extends': {
-            'blockToolbar': [
-                'paragraph', 'heading1', 'heading2', 'heading3', '|',
-                'bulletedList', 'numberedList', '|', 'blockQuote'
-            ],
-            'toolbar': [
-                'heading', '|', 'outdent', 'indent', '|', 'bold',
-                'italic', 'link', 'underline', 'strikethrough', 'code',
-                'subscript', 'superscript', 'highlight', '|', 'codeBlock',
-                'sourceEditing', 'insertImage', 'bulletedList', 'numberedList',
-                'todoList', '|', 'blockQuote', 'imageUpload', '|', 'fontSize',
-                'fontFamily', 'fontColor', 'fontBackgroundColor', 'mediaEmbed',
-                'removeFormat', 'insertTable',
-            ],
-            'image': {
-                'toolbar': [
-                    'imageTextAlternative', '|', 'imageStyle:alignLeft',
-                    'imageStyle:alignRight', 'imageStyle:alignCenter',
-                    'imageStyle:side', '|'
-                ],
-                'styles': [
-                    'full', 'side', 'alignLeft', 'alignRight', 'alignCenter'
-                ]
-            },
-            'table': {
-                'contentToolbar': [
-                    'tableColumn', 'tableRow', 'mergeTableCells',
-                    'tableProperties', 'tableCellProperties'
-                ]
-            },
-            'heading': {
-                'options': [
-                    {
-                        'model': 'paragraph',
-                        'title': 'Paragraph',
-                        'class': 'ck-heading_paragraph'
-                    },
-                    {
-                        'model': 'heading1',
-                        'view': 'h1',
-                        'title': 'Heading 1',
-                        'class': 'ck-heading_heading1'
-                    },
-                    {
-                        'model': 'heading2',
-                        'view': 'h2', 'title':
-                        'Heading 2', 'class':
-                        'ck-heading_heading2'},
-                    {
-                        'model': 'heading3',
-                        'view': 'h3', 'title':
-                        'Heading 3', 'class':
-                        'ck-heading_heading3'
-                    }
-                ]
-            }
-        },
-        'list': {
-            'properties': {
-                'styles': 'true',
-                'startIndex': 'true',
-                'reversed': 'true',
-            }
-        }
-    }
+SUMMERNOTE_THEME = 'bs4'  # Show summernote with Bootstrap4
+
+SUMMERNOTE_CONFIG = {
+    'iframe': False,
+    
+     'width': '100%',
+        'height': '480',
 }
 
 MIDDLEWARE = [
