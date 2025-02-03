298f46f6f2d8c9d241dcabf6426596a1093e3b17
Charlie <c.enright@me.com>
2025-02-03 17:34:51 +0000

    Bug: Remove login authentication requirement for checkout confirmation

--- botanic_labs/settings.py
+++ botanic_labs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 APPEND_SLASH = True
 
 # Debug settings
-DEBUG = False  # Set to False in production
+DEBUG = True  # Set to False in production
 
 # Uncomment for deployment:
 DEBUG = 'DEVELOPMENMT' in os.environ
@@ -201,11 +201,23 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
+    # Production: Serve from AWS S3
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+else:
+    # Local: Serve from the static folder
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'botanic-labs'
